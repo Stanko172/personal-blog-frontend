8beea872146f5b492eb3b1e11eb1d5ce3f9e7574
Stanko172 <stanko.bebek00@gmail.com>
2023-12-27 12:58:12 +0100

    fix: render email fragment only on client

--- components/ContactMe.vue
+++ components/ContactMe.vue
@@ -21,14 +21,16 @@ const { copy, copied, isSupported } = useClipboard()
       </a>
     </div>
     <hr class="my-3 border-brand-outline">
-    <div class="relative flex group items-center justify-center gap-2 w-max cursor-pointer">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-5 h-5 mt-1 stroke-brand-primary"><path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /></svg>
-      <button class="no-underline text-brand-secondary text-sm" @click="copy(email)">
-        {{ email }}
-      </button>
-      <span v-if="isSupported" class="pointer-events-none absolute -top-7 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100 text-xs bg-brand-outline px-2 py-1">
-        {{ copied ? 'Copied!' : 'Click to copy' }}
-      </span>
-    </div>
+    <ClientOnly>
+      <div class="relative flex group items-center justify-center gap-2 w-max cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-5 h-5 mt-1 stroke-brand-primary"><path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /></svg>
+        <button class="no-underline text-brand-secondary text-sm" @click="copy(email)">
+          {{ email }}
+        </button>
+        <span v-if="isSupported" class="pointer-events-none absolute -top-7 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100 text-xs bg-brand-outline px-2 py-1">
+          {{ copied ? 'Copied!' : 'Click to copy' }}
+        </span>
+      </div>
+    </ClientOnly>
   </div>
 </template>
