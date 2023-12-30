a093a60d4e171266e0537e3a301008921e5229cf
Stanko172 <stanko.bebek00@gmail.com>
2023-12-30 15:42:11 +0100

    feat: add initial about me page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,6 +2,54 @@
 
 <template>
   <Page title="About">
-    <EmptyCard />
+    <Paragraph>
+      Hello there! 👋
+
+      <br><br>
+
+      I'm Stanko, a full-stack software developer with a love for making cool stuff on the internet.
+
+      My coding adventure started back in high school. Picture this: playing with Lego robots and Arduino gadgets, and suddenly, I saw lines of code doing magical things. It blew my mind and sparked my interest in programming.
+
+      I taught myself the ropes by reading online articles and watching YouTube videos. Python became my playground, and I started creating small programs and games. Living in a small town without a big community of developers was a bit tricky, but I kept at it.
+
+      <br><br>
+
+      Next stop: the Faculty of Science and Education at the University of Mostar. From 2018 to 2021, I studied Computer Science and met some fantastic people. We run a student group called "Switch" and did cool things like organizing events and participating in hackathons.
+
+      We've organized meetups, code challenges, workshops and built a lot projects. We've also participated in programs like "Huawei Seeds for the Future", "Microsoft NetWork" conference, BEST hackathon and more.
+
+      <br><br>
+
+      <img src="/switch.jpeg">
+
+      <br>
+
+      I wore different hats, from teaching programming basics to being the head of the Student Council. Winning a Smart Parking competition and presenting my thesis on "Developing Chess AI Systems" were big moments. Along the way, I got into using Laravel and Vue.js, tools that I still use today.
+
+      <br><br>
+
+      <img src="/smart-campus.jpeg">
+
+      <br>
+
+      In the summer of 2021, I landed an internship at QSD, where I worked on an online library project. It was a blast, and I learned a ton about startups, business, and making cool things on the web.
+
+      <br><br>
+
+      Come November 2021, I joined ZenDev and worked on projects like Rozbinize, an AI tool for content writers. It was a game-changer, and I learned heaps from the awesome people at ZenDev.
+
+      <br><br>
+
+      <img src="/zendev-team-building.jpeg">
+
+      <br>
+
+      Now, I'm working as full-stack developer for Returnless, a Dutch company making returns hassle-free.
+
+      <br><br>
+
+      In my free time, I'm all about sports (huge fan of HNK Hajduk Split!), running, music, and exploring. I've loved reading and writing since I was a kid, so I spend some of my free time creating technical content.
+    </Paragraph>
   </Page>
 </template>
