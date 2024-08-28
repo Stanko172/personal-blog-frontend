251cfe22456a9c1acc33309613dbbb93313af5ff
Stanko172 <stanko.bebek00@gmail.com>
2024-08-28 14:50:06 +0200

    about me and empty card content update

--- components/EmptyCard.vue
+++ components/EmptyCard.vue
@@ -6,8 +6,8 @@ import { FontSize } from '~/enums'
   <div class="p-4 sm:p-6 text-center rounded border border-brand-outline bg-brand-surface shadow-lg">
     <Icon name="openmoji:space-shuttle" class="w-12 h-12 mb-2" />
     <Paragraph :font-size="FontSize.SMALL">
-      In the vast digital quiet, an empty canvas awaits. <br>
-      No written words, just the cool promise of my story ready to unfold in this serene space.
+      In the vast digital realm, an empty canvas awaits. <br>
+      No words have been written yet, just the quiet promise of a story ready to unfold.
     </Paragraph>
   </div>
 </template>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -13,15 +13,19 @@ import { FontSize, HeadingElement, HyperlinkTarget } from '~/enums'
 
         <br><br>
 
-        I'm Stanko, a full-stack software developer with a love for making cool stuff on the internet.
-
-        My coding adventure started back in high school. Picture this: playing with Lego mindstorms and Arduino gadgets, and suddenly, I saw lines of code doing magical things. It blew my mind and sparked my interest in programming.
-
-        I taught myself the ropes by reading online articles and watching YouTube videos. Python became my playground, and I started creating small programs and games. Living in a small town without a big community of developers was a bit tricky, but I kept at it.
+        I'm Stanko, a full-stack software developer who loves making cool stuff on the internet.
 
         <br><br>
 
-        My next stop is the <Hyperlink to="https://fpmoz.sum.ba/" :target="HyperlinkTarget.BLANK">
+        My coding adventure started in high school. 
+        
+        I attended a couple of workshops where I played with Lego Mindstorms, Arduinos, and similar gadgets. 
+        
+        When I wrote my first lines of code, it completely blew my mind.
+
+        <br><br>
+
+        My next stop was the <Hyperlink to="https://fpmoz.sum.ba/" :target="HyperlinkTarget.BLANK">
           Faculty of Science and Education
         </Hyperlink> at the University of Mostar. From 2018 to 2021, I studied Computer Science and met some fantastic people. We run a student group called <Hyperlink to="https://switch.sum.ba/" :target="HyperlinkTarget.BLANK">
           Switch
@@ -58,7 +62,7 @@ import { FontSize, HeadingElement, HyperlinkTarget } from '~/enums'
           ZenDev
         </Hyperlink> and worked on projects like <Hyperlink to="https://robinize.com/" :target="HyperlinkTarget.BLANK">
           Robinize
-        </Hyperlink>, an AI tool for content writers. It was a game-changer, and I learned a lot from the awesome people at ZenDev. That was my first real-world project and represented a stepping stone for me. Also, I worked on a project called Open Hours, where we managed a database with information for over a million places in the world. The project was built with Python and MongoDB.
+        </Hyperlink>, an AI tool for content writers. I learned a lot from the awesome people at ZenDev. That was my first real-world project and represented a stepping stone for me. Also, I worked on a project called Open Hours, where we managed a database with information for over a million places in the world. The project was built with Python and MongoDB.
 
         <br><br>
 
@@ -66,9 +70,17 @@ import { FontSize, HeadingElement, HyperlinkTarget } from '~/enums'
 
         <br>
 
-        Now, I'm working as a full-stack developer for <Hyperlink to="https://www.returnless.com/" :target="HyperlinkTarget.BLANK">
+        From 2022 to 2024, I worked as a full-stack developer for <Hyperlink to="https://www.returnless.com/" :target="HyperlinkTarget.BLANK">
           Returnless
-        </Hyperlink>, a Dutch company that makes returns hassle-free. Returnless is an all-in-one Return Platform to handle returns faster, lower costs and prevent future returns. Here, I'm serving the role of a full-stack developer, mainly working with Laravel and VueJs.
+        </Hyperlink>, a Dutch company that makes returns hassle-free. Returnless is an all-in-one Return Platform to handle returns faster, lower costs and prevent future returns. Here, I was serving the role of a full-stack developer, mainly working with Laravel and VueJs.
+
+        <br><br>
+
+        Since February 2024, I have been a full-stack (Laravel and Vue JS) developer at <Hyperlink to="https://avidnote.com/" :target="HyperlinkTarget.BLANK">Avidnote</Hyperlink>. Avidnote enables faster writing, reading research papers, and analyzing research data with AI templates.
+
+        <br><br>
+
+        Meanwhile, I was also a part of the development team for the <Hyperlink to="https://www.niosaas.com/" :target="HyperlinkTarget.BLANK">NIO SaaS MVP</Hyperlink>. I had the pleasure of working with FastAPI and Vue JS while building the first version of an intelligent agenda / strategic planning application for hitting revenue and margin targets efficiently.
 
         <br><br>
 
