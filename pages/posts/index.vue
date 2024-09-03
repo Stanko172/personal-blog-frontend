d732c558a4ea9eb91d4d754d0fdd319b675222e4
Stanko172 <stanko.bebek00@gmail.com>
2024-09-03 19:55:12 +0200

    fetch all content types

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import { FontSize, HeadingElement } from '~/enums'
+
+const config = useRuntimeConfig()
+
+const { data } = useFetch('/contents?type=post', {
+  baseURL: config.public.apiBaseUrl
+});
 </script>
 
 <template>
@@ -14,7 +20,11 @@ import { FontSize, HeadingElement } from '~/enums'
       <Paragraph>
         Tech Trek: Geek Speak, Software Feats, and a Dash of Digital Wizardry.
       </Paragraph>
-      <EmptyCard />
+      <EmptyCard v-if="data?.data.length === 0"/>
+      <ContentList 
+        v-else
+        :contents="data?.data"
+      />
     </Stack>
   </Page>
 </template>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import { FontSize, HeadingElement } from '~/enums'
+
+const config = useRuntimeConfig()
+
+const { data } = useFetch('/contents?type=project', {
+  baseURL: config.public.apiBaseUrl
+});
 </script>
 
 <template>
@@ -14,7 +20,11 @@ import { FontSize, HeadingElement } from '~/enums'
       <Paragraph>
         Creative Exploits: Showcasing My Diverse Spectrum of Projects.
       </Paragraph>
-      <EmptyCard />
+      <EmptyCard v-if="data?.data.length === 0"/>
+      <ContentList 
+        v-else
+        :contents="data?.data"
+      />
     </Stack>
   </Page>
 </template>
