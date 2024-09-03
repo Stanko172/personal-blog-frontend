1234dda26d8e88862b81e1610b6462aad2da0fa0
Stanko172 <stanko.bebek00@gmail.com>
2024-09-03 20:10:45 +0200

    fix image width and height

--- components/content/Item.vue
+++ components/content/Item.vue
@@ -14,20 +14,18 @@ const props = defineProps<{
         <img 
             :src="content.cover_image" 
             :alt="content.title" 
-            class="my-10 overflow-hidden rounded border border-brand-outline !my-0 w-full md:w-1/2" 
-            width="570" 
-            height="321" 
+            class="rounded border border-brand-outline w-[260px] object-cover" 
             loading="lazy" 
             decoding="async"
         >
         <div class="flex flex-col gap-2">
             <span class="flex flex-col sm:flex-row gap-2 sm:items-center">
-            <time class="text-xs font-bold text-brand-primary">{{ content.published_at }}</time>
+                <time class="text-xs font-bold text-brand-primary">{{ content.published_at }}</time>
             </span>
             <strong class="block text-brand-text text-xl ">{{ content.title }}</strong>
             <p class="font-light text-sm ">{{ content.content }}</p> 
             <ul class="flex flex-wrap items-baseline gap-2 list-none m-0 p-0 mt-2 mb-0"> 
-                <li class="m-0 p-0 rounded text-xs text-brand-salmon border-brand-outline border border-brand-outline bg-brand-surface">
+                <li class="m-0 p-0 rounded text-xs text-brand-salmon border border-brand-outline bg-brand-surface">
                     <strong class="block px-2 py-1 font-medium text-brand-tertiary">{{ content.type }}</strong>
                 </li>
             </ul> 
